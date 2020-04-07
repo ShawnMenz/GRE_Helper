a42a55b7893fc56ae0965c23b7a107c6bf432f6c
Shawn <shawnmenz@qq.com>
2020-04-07 20:17:44 +0800

    first commit

---
+++ GRE_UI.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GRE_UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Frame(object):
+    def setupUi(self, Frame):
+        Frame.setObjectName("Frame")
+        Frame.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(Frame)
+        self.centralwidget.setObjectName("centralwidget")
+        self.word = QtWidgets.QLabel(self.centralwidget)
+        self.word.setGeometry(QtCore.QRect(200, 80, 400, 40))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.word.setFont(font)
+        self.word.setAlignment(QtCore.Qt.AlignCenter)
+        self.word.setObjectName("word")
+        self.explain = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.explain.setGeometry(QtCore.QRect(100, 150, 600, 300))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.explain.setFont(font)
+        self.explain.setReadOnly(True)
+        self.explain.setObjectName("explain")
+        self.showButton = QtWidgets.QPushButton(self.centralwidget)
+        self.showButton.setGeometry(QtCore.QRect(100, 480, 100, 30))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.showButton.setFont(font)
+        self.showButton.setObjectName("showButton")
+        self.coverButton = QtWidgets.QPushButton(self.centralwidget)
+        self.coverButton.setGeometry(QtCore.QRect(350, 480, 100, 30))
+        self.coverButton.setObjectName("coverButton")
+        self.nextButton = QtWidgets.QPushButton(self.centralwidget)
+        self.nextButton.setGeometry(QtCore.QRect(600, 480, 100, 30))
+        self.nextButton.setObjectName("nextButton")
+        self.count = QtWidgets.QLabel(self.centralwidget)
+        self.count.setGeometry(QtCore.QRect(350, 530, 100, 20))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.count.setFont(font)
+        self.count.setObjectName("count")
+        Frame.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(Frame)
+        self.statusbar.setObjectName("statusbar")
+        Frame.setStatusBar(self.statusbar)
+
+        self.retranslateUi(Frame)
+        QtCore.QMetaObject.connectSlotsByName(Frame)
+
+    def retranslateUi(self, Frame):
+        _translate = QtCore.QCoreApplication.translate
+        Frame.setWindowTitle(_translate("Frame", "GRE_Helper"))
+        self.word.setText(_translate("Frame", "TextLabel"))
+        self.showButton.setText(_translate("Frame", "Show"))
+        self.coverButton.setText(_translate("Frame", "Cover"))
+        self.nextButton.setText(_translate("Frame", "Next"))
+        self.count.setText(_translate("Frame", "Count:"))
---
+++ GRE_helper.py
@@ -0,0 +1,48 @@
+import xlrd
+import random
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+from GRE_UI import Ui_Frame
+
+
+class GRE_Helper(QtWidgets.QMainWindow, Ui_Frame):
+    def __init__(self):
+        self.file = xlrd.open_workbook("data/GRE.xlsx")
+        self.table = self.file.sheet_by_name("GRE")
+        self.seed = random.randint(0, self.table.nrows - 1)
+        self.row = self.table.row_values(self.seed)
+        self.num = 1
+        super(GRE_Helper, self).__init__()
+        self.setupUi(self)
+        self.showButton.clicked.connect(lambda: self.show_word())
+        self.coverButton.clicked.connect(lambda: self.cover_word())
+        self.nextButton.clicked.connect(lambda: self.next_word())
+        self.word.setText(str(self.row[0]))
+        self.count.setText("Count: 1")
+
+    def show_word(self):
+        exp = ""
+        length = len(self.row)
+        for i in range(1, length, 2):
+            temp = str(self.row[i]) + " " + str(self.row[i + 1]) + "\n"
+            exp += temp
+        self.explain.setPlainText(exp)
+
+    def cover_word(self):
+        self.explain.setPlainText("")
+
+    def next_word(self):
+        self.seed = random.randint(0, self.table.nrows - 1)
+        self.row = self.table.row_values(self.seed)
+        self.word.setText(str(self.row[0]))
+        self.explain.setPlainText("")
+        self.num += 1
+        self.count.setText("Count: %d" % self.num)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = GRE_Helper()
+    ui.show()
+    sys.exit(app.exec_())
